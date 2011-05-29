febb3ec8d64d0af93e78466714175ca9f3d7b26c
psvensson <psvensson@gmail.com>
2011-05-29 18:29:05 +0000

    Added first prototype of connector line drawing

---
+++ drawutil/port.js
@@ -0,0 +1,76 @@
+dojo.provide("drawutil.port");
+
+
+dojo.require("dojox.gfx");
+
+dojo.declare("drawutil.port",  null ,
+{    
+    surface             : "",
+    side                : 10,
+    x                   : 0,
+    y                   : 0,
+
+    constructor: function(args)
+    {
+        this.inherited(arguments);
+        console.log("drawutil.port constructor. args are "+args);
+        this.x = args.x;
+        this.y = args.y;
+        this.where = args.where;
+        this.position = args.position;
+        this.surface = args.surface;
+    },
+    
+    remove: function()
+    {
+        console.log("drawutil.port remove called..");
+      if(this.avatar)
+      {
+        this.surface.remove(this.avatar);   
+      }
+    },
+    
+    render: function(x, y)
+    {
+        this.x = x;
+        this.y = y;
+        //console.log("drawutil.port render called. x = "+x+", y = "+y);
+        var rect = {x: x, y: y, width: this.side, height: this.side};
+        //console.log("creating port with parameters..");
+        //console.dir(rect);
+        var red_rect = this.surface.createRect(rect);
+        red_rect.setFill("white");
+        red_rect.setStroke({color: "green", width: 1}); 
+        this.avatar = red_rect;
+        dojo.connect(red_rect.getNode(), "onclick", this, this.startConnect);
+        //'this.surface.add(this.avatar);
+    },
+    
+    startConnect: function(e)
+    {
+        console.log("startconnect.:");
+        this.drawLineEvent = this.surface.connect("onmousemove", this, this.drawLine);
+        dojo.connect(this.surface, "onmouseup", this, this.stopConnect);
+        dojo.connect(this.surface, "ondragstart",   dojo, "stopEvent");
+        dojo.connect(this.surface, "onselectstart", dojo, "stopEvent");
+    },
+    
+    drawLine: function(e)
+    {
+        console.log("drawLine..");
+        console.dir(e);
+          if(this.line)
+          {
+            this.surface.remove(this.line);
+            this.line = null;
+          }
+          var ll = {x1: this.x, y1: this.y, x2: e.clientX, y2: e.clientY};
+          this.line = this.surface.createLine(ll);
+          this.line.setStroke({color: "red", width: 1});
+    },
+    
+    stopConnect: function(e)
+    {
+           
+    }
+});
