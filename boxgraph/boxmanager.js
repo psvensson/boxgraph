ce038cb28d2d6966f524290ae3837bf95a57fb2c
psvensson <psvensson@gmail.com>
2011-08-12 10:45:47 +0000

    added boxmanager to make easier routing possible

---
+++ boxgraph/boxmanager.js
@@ -0,0 +1,23 @@
+dojo.provide("boxgraph.boxmanager");
+
+
+dojo.declare("boxgraph.boxmanager", null,
+{
+    boxes:      [],
+    
+    constructor: function()
+    {
+        this.boxes = [];  
+    },
+    
+    addBox: function(box)
+    {
+        
+    },
+    
+    getBoxes: function()
+    {
+        return this.boxes;   
+    }
+    
+});
