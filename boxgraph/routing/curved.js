78957eb7e08c401ad4ae7a19ef0d35a14056f360
psvensson <psvensson@gmail.com>
2011-08-31 12:15:25 +0000

    quick fix for curved

--- boxgraph/routing/curved.js
+++ boxgraph/routing/curved.js
@@ -1,5 +1,7 @@
 dojo.provide("boxgraph.routing.curved");
 
+dojo.require("boxgraph.routing.manhattan2");
+
 dojo.declare("boxgraph.routing.curved", null,
 {
 	getRouting: function(firstport, secondport)
--- boxgraph/test/testBezier.js
+++ boxgraph/test/testBezier.js
@@ -3,11 +3,10 @@ dojo.provide("drawutil.test.testBezier");
 dojo.require("dijit._Templated");
 dojo.require("dijit._Widget");
 
-dojo.require("drawutil.base");
 dojo.require("dojox.gfx");
 
 
-dojo.declare("drawutil.test.testBezier", [	dijit._Widget, dijit._Templated, drawutil.base ],
+dojo.declare("drawutil.test.testBezier", [	dijit._Widget, dijit._Templated  ],
 {
 	templatePath			: dojo.moduleUrl("drawutil.test", "testBezier.html"),
 	widgetsInTemplate	: true,
@@ -69,6 +68,39 @@ dojo.declare("drawutil.test.testBezier", [	dijit._Widget, dijit._Templated, draw
 
 		this.circle.setTransform({x: x, y: y});
 
+	},
+    
+    getBezierFor: function(percent, C1, C2, C3, C4)
+	{
+		//====================================\\
+		// 13thParallel.org Beziér Curve Code \\
+		//   by Dan Pupius (www.pupius.net)   \\
+		//====================================\\
+
+
+		function B1(t) { return t*t*t }
+		function B2(t) { return 3*t*t*(1-t) }
+		function B3(t) { return 3*t*(1-t)*(1-t) }
+		function B4(t) { return (1-t)*(1-t)*(1-t) }	
+
+		var pos = {x:0, y:0};
+		pos.x = C1.x*B1(percent) + C2.x*B2(percent) + C3.x*B3(percent) + C4.x*B4(percent);
+		pos.y = C1.y*B1(percent) + C2.y*B2(percent) + C3.y*B3(percent) + C4.y*B4(percent);
+		return pos;
+
+	},
+
+	drawBezier: function(C1, C2, C3, C4, numPixels, objs)
+	{
+		for(var i=0; i<numPixels; i++)
+		{				
+			percent = (1/numPixels) * i;
+			var pos = this.getBezierFor(percent, C1, C2, C3, C4);
+			var x = parseInt(pos.x);
+			var y = parseInt(pos.y);
+			console.log(" transforming "+objs[i]+" to "+x+", "+y);
+			objs[i].setTransform([dojox.gfx.matrix.translate(x, y)]);
+		}
 	}
 
 
